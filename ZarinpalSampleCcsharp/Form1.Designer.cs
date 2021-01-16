db034b27607a50136b7771e9fe373c3a8fe808c5
erfandn <erfanad1371@gmail.com>
2021-01-16 13:21:30 +0330

    Add project files.

---
+++ ZarinpalSampleCcsharp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ZarinpalSampleCcsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnrequest = new System.Windows.Forms.Button();
+            this.lblrequest = new System.Windows.Forms.Label();
+            this.btnverify = new System.Windows.Forms.Button();
+            this.lblverify = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnrequest
+            // 
+            this.btnrequest.Location = new System.Drawing.Point(26, 12);
+            this.btnrequest.Name = "btnrequest";
+            this.btnrequest.Size = new System.Drawing.Size(75, 23);
+            this.btnrequest.TabIndex = 0;
+            this.btnrequest.Text = "request";
+            this.btnrequest.UseVisualStyleBackColor = true;
+            this.btnrequest.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblrequest
+            // 
+            this.lblrequest.AutoSize = true;
+            this.lblrequest.Location = new System.Drawing.Point(23, 57);
+            this.lblrequest.Name = "lblrequest";
+            this.lblrequest.Size = new System.Drawing.Size(70, 13);
+            this.lblrequest.TabIndex = 2;
+            this.lblrequest.Text = "request result";
+            // 
+            // btnverify
+            // 
+            this.btnverify.Location = new System.Drawing.Point(270, 12);
+            this.btnverify.Name = "btnverify";
+            this.btnverify.Size = new System.Drawing.Size(75, 23);
+            this.btnverify.TabIndex = 3;
+            this.btnverify.Text = "verify";
+            this.btnverify.UseVisualStyleBackColor = true;
+            this.btnverify.Click += new System.EventHandler(this.btnverify_Click);
+            // 
+            // lblverify
+            // 
+            this.lblverify.AutoSize = true;
+            this.lblverify.Location = new System.Drawing.Point(267, 110);
+            this.lblverify.Name = "lblverify";
+            this.lblverify.Size = new System.Drawing.Size(32, 13);
+            this.lblverify.TabIndex = 4;
+            this.lblverify.Text = "verify";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 336);
+            this.Controls.Add(this.lblverify);
+            this.Controls.Add(this.btnverify);
+            this.Controls.Add(this.lblrequest);
+            this.Controls.Add(this.btnrequest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnrequest;
+        private System.Windows.Forms.Label lblrequest;
+        private System.Windows.Forms.Button btnverify;
+        private System.Windows.Forms.Label lblverify;
+    }
+}
+
---
+++ ZarinpalSampleCcsharp/Form1.cs
@@ -0,0 +1,187 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZarinpalSampleCcsharp
+{
+    public partial class Form1 : Form
+    {
+        //in nemoone code ba estandarde Rest va mostanadate jadaid tahiye shode ast
+        //ketabkhane haye morede niaz ***NewtonSoft.json*** va ***RestSharp*** ast ke az nuggetpackagemanager mitavanid nasb konid
+
+        //ketabkhaneye restsharp be dalile sadegi dar piadesazi dar in nemoone code estefade shode ast
+        //dar file code.txt piade sazi az tarighe HttpWebRequest niz anjam shode ast ke dar soorate tamayol mitavanid estefade konid
+
+
+        string merchant = "424baadf-ea4c-4744-b29e-5eb62a855821";
+        string amount = "10000";
+        string authority;
+        string description = "خرید تستی ";
+        string callbackurl = "http://www.erfandn.ir";
+      
+        
+       
+        public Form1()
+        {
+           
+            InitializeComponent();
+    
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+   
+            try
+            {
+
+                string[] metadata=new string[2];
+                metadata[0] = "[mobile: 09121234567]";
+                metadata[1] = "[email: info.test@gmail.com]";
+
+                //be dalil in ke metadata be sorate araye ast va do meghdare mobile va email dar metadata gharar mmigirad
+                //shoma mitavanid in maghadir ra az kharidar begirid va set konid dar gheir in sorat khali ersal konid
+
+                string requesturl;
+                requesturl = "https://api.zarinpal.com/pg/v4/payment/request.json?merchant_id=" +
+                    merchant + "&amount=" + amount +
+                    "&callback_url=" + callbackurl +
+                    "&description=" + description +
+                    "&metadata[0]=" + metadata[0] + "& metadata[1]=" + metadata[1];
+;
+
+
+                var client = new RestClient(requesturl);
+
+                client.Timeout = -1;
+
+                var request = new RestRequest(Method.POST);
+
+                request.AddHeader("accept", "application/json");
+
+                request.AddHeader("content-type", "application/json");
+
+                IRestResponse requestresponse = client.Execute(request);
+
+                Newtonsoft.Json.Linq.JObject jo = Newtonsoft.Json.Linq.JObject.Parse(requestresponse.Content);
+                string errorscode = jo["errors"].ToString();
+
+                Newtonsoft.Json.Linq.JObject jodata = Newtonsoft.Json.Linq.JObject.Parse(requestresponse.Content);
+                string dataauth = jodata["data"].ToString();
+  
+                if (dataauth != "[]")
+                {
+                    
+                    authority = jodata["data"]["authority"].ToString();
+
+                    lblrequest.Text = "data = " + authority;
+
+                    string gatewayUrl = "https://www.zarinpal.com/pg/StartPay/" + authority;
+                    try
+                    {
+                        System.Diagnostics.Process.Start(gatewayUrl);
+                    }
+                    catch (System.ComponentModel.Win32Exception noBrowser)
+                    {
+                        if (noBrowser.ErrorCode == -2147467259)
+                            MessageBox.Show(noBrowser.Message);
+                    }
+                    catch (System.Exception other)
+                    {
+                        MessageBox.Show(other.Message);
+                    }
+
+                }
+                else
+                {
+
+                    MessageBox.Show("errors" + errorscode);
+
+                }
+            
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Wrong request ! " + ex.Message, "Error");
+            }
+        }
+
+        private void btnverify_Click(object sender, EventArgs e)
+        {
+
+
+            try {
+
+                string authorityverify = authority;
+
+               // lblverify.Text = authorityverify;
+
+                ////////////////////////////////////////////////////////////////////////////////////
+                string url = "https://api.zarinpal.com/pg/v4/payment/verify.json?merchant_id=" +
+                    merchant + "&amount="
+                    + amount + "&authority="
+                    + authorityverify;
+
+                var client = new RestClient(url);
+                client.Timeout = -1;
+
+                var request = new RestRequest(Method.POST);
+
+                request.AddHeader("accept", "application/json");
+
+                request.AddHeader("content-type", "application/json");
+
+                IRestResponse response = client.Execute(request);
+
+
+                Newtonsoft.Json.Linq.JObject jodata = Newtonsoft.Json.Linq.JObject.Parse(response.Content);
+                string data = jodata["data"].ToString();
+
+                Newtonsoft.Json.Linq.JObject jo = Newtonsoft.Json.Linq.JObject.Parse(response.Content);
+                string errors = jo["errors"].ToString();
+
+                if (data != "[]")
+                {
+                    string refid = jodata["data"]["ref_id"].ToString();
+                    lblverify.Text = "تراکنش موفق بوده است با شناسه " + refid;
+                }
+                else if (errors != "[]")
+                {
+
+                    string errorscode = jo["errors"]["code"].ToString();
+
+                    lblverify.Text = "تراکنش ناموفق بوده است با ارور " + errorscode;
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                lblverify.Text = ex.ToString();
+
+            }
+
+            
+
+
+        
+
+        }
+    }
+  
+
+}
